f8f195296e624053ad27a387be3881e33860cd53
Guillaume Tremblay <guiblayz@hotmail.com>
2022-10-31 11:44:26 -0400

    Ajout de class project-Guillaume

---
+++ src/Projet_Agile/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Agile
+{
+    class Project
+    {
+       public int idProject
+        {
+            get;
+            set;
+        }
+        public int codeProject
+        {
+            get;
+            set;
+        }
+        public Project(int idProject,int codeProject)
+        {
+            this.idProject = idProject;
+            this.codeProject = codeProject;
+        }
+    }
+}
