af93379f251acfbf8cad71a71f65a43a5e4c8974
Martin forget <mforget2011@hotmail.com>
2022-11-02 19:35:26 -0400

    réinitialisation admin et employe

--- src/Projet_Agile/Employe.cs
+++ src/Projet_Agile/Employe.cs
@@ -10,11 +10,6 @@ namespace Projet_Agile
     {
         public Employe(int noUser, string lastName, string firstName, string email, string password, string inscriptionDate) : base (lastName, firstName, email, password)
         {
-            if(noUser < 1000)
-            {
-                Console.WriteLine("Le numéro de user est invalide pour cette catégorie d'employé");
-            }
-            else
                 this.noUser = noUser;
         }
 
