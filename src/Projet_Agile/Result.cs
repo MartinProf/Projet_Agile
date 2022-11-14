37ead2773475d715eba99bfdc2178c9c989ed9fd
MercyForKohai <loljetetroll@hotmail.com>
2022-11-13 21:30:20 -0500

    Ajout de la classe Result, Modification de Timesheet.cs : Ajout des methodes createOrAddToResultJson et readResultJson. Mise en commentaire des Console write lines pour les test unitaires.

---
+++ src/Projet_Agile/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Agile
+{
+    public class Result
+    {
+        public int numEmploye { get; set; }
+        public List<string> errorCodes { get; set; }
+    }
+}
