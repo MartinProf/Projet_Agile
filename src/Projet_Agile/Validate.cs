519795dd31976cf35a1e8a817f6fcdbb6954ed4a
Martin forget <mforget2011@hotmail.com>
2022-11-16 14:25:38 -0500

    cleanup

--- src/Projet_Agile/Validate.cs
+++ src/Projet_Agile/Validate.cs
@@ -19,7 +19,7 @@ namespace Projet_Agile
         string text5 = "";
         string text6 = "";
         string fileName = Program.extensionFile;
-        string extension = System.IO.Path.GetExtension(Program.extensionFile);
+        string extension = Path.GetExtension(Program.extensionFile);
         public Validate() { }
         [JsonProperty("numero employe")]
         public int empNumber
@@ -100,7 +100,6 @@ namespace Projet_Agile
 
                     if (minutes < (36 * 60))
                     {
-                        //Console.WriteLine("L'administrateur : #" + empNumber + " n'a pas travaillé le minimum d'heures requises au bureau. Il en a travaillé " + minutes / 60 + " sur 36 minimum.");
                         text1 = "L'administrateur : #" + empNumber + " n'a pas travaillé le minimum d'heures requises au bureau. Il en a travaillé " + minutes / 60 + " sur 36 minimum.";
                         createOrAddToResultJson(fileName);
 
@@ -133,7 +132,6 @@ namespace Projet_Agile
                     }
                     if (minutes < (38 * 60))
                     {
-                        //Console.WriteLine("L'employé normal : #" + empNumber + " n'a pas travaillé le minimum d'heures requises au bureau. Il en a travaillé " + minutes / 60 + " sur 38 minimum.");
                         text2 = "L'employé normal : #" + empNumber + " n'a pas travaillé le minimum d'heures requises au bureau. Il en a travaillé " + minutes / 60 + " sur 38 minimum.";
                         createOrAddToResultJson(fileName);
                     }
@@ -164,7 +162,6 @@ namespace Projet_Agile
                     }
                     if (minutesPerDay < 360)
                     {
-                        //Console.WriteLine("L'employé normal : #" + empNumber + " n'a pas travaillé le minimum de temps requis (360 minutes) lundi!   temps travaillé ce jour-ci: " + minutesPerDayMonday + " Minutes.");
                         text3 += "L'employé normal : #" + empNumber + " n'a pas travaillé le minimum de temps requis (360 minutes) " + timeSt.DayName(jour) + "! temps travaillé ce jour-ci: " + minutesPerDay + " Minutes. ";
                         createOrAddToResultJson(fileName);
                     }
@@ -188,7 +185,6 @@ namespace Projet_Agile
                     }
                     if (minutesPerDay < 240)
                     {
-                        // Console.WriteLine("L'employé normal : #" + empNumber + " n'a pas travaillé le minimum de temps requis (240 minutes) le lundi!   temps travaillé ce jour-ci: " + minutesPerDayMonday + " Minutes.");
                         text4 += "L'employé normal : #" + empNumber + " n'a pas travaillé le minimum de temps requis (240 minutes) " + timeSt.DayName(jour) + "! temps travaillé ce jour-ci: " + minutesPerDay + " Minutes. ";
                         createOrAddToResultJson(fileName);
                     }
@@ -210,7 +206,6 @@ namespace Projet_Agile
             }
             if (weeklytotal > 2580)
             {
-                //Console.WriteLine("L'employé a depassé le temps de travaul permis au bureau (43heures / 2580 minutes)!   temps travaillé ce jour-ci: " + weeklytotal + " Minutes.");
                 text5 = "L'employé a depassé le temps de travail permis au bureau (43heures / 2580 minutes)!   temps travaillé ce jour-ci: " + weeklytotal + " Minutes.";
                 createOrAddToResultJson(fileName);
             }
@@ -237,7 +232,6 @@ namespace Projet_Agile
                     }
                     if (minutes > (10 * 60))
                     {
-                        //Console.WriteLine("L'administrateur : #" + empNumber + " a dépassé le nombre d'heures permises en télétravail. Il en a travaillé " + minutes / 60 + " sur une possibilité de 10 maximum.");
                         text6 = "L'administrateur : #" + empNumber + " a dépassé le nombre d'heures permises en télétravail. Il en a travaillé " + minutes / 60 + " sur une possibilité de 10 maximum.";
                         createOrAddToResultJson(fileName);
                     }
