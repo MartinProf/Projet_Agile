9bff9f4e36425f76c4c0cb5d71737461b6183663
Sebastiant1012 <sebastiant1012@gmail.com>
2022-11-25 19:47:03 -0500

    Creation de la structure du Projet avec interface graphique
    j'ai crée les Fenêtres Login, Manager et Employee. les composant visiuels et fonctionelles dont on va s'en servir.
    
    J'ai ajouté les fonctionalités de Login/Logout. et Le bouttons Add dans le jour de la semaine

--- src/PunchClock/Manager.aspx.cs
+++ src/PunchClock/Manager.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace PunchClock
 {
-    public partial class Accueil : System.Web.UI.Page
+    public partial class Manager : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
